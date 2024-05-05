fd715e82ad5f2b7e219e4bf0912841d5ee0af172
Dmytro Holub <heap3d@gmail.com>
2024-05-05 12:41:11 +0300

    v1.1 updated: decode_name() supports advanced parameters

--- scripts/cable_setup.py
+++ scripts/cable_setup.py
@@ -20,7 +20,17 @@ from h3d_utilites.scripts.h3d_debug import H3dDebug
 
 CABLE_SHAPE_NAME = 'h3d_cable_shape_circle'
 DEFAULT_DIAMETER = 0.01
+DEFAULT_MATERIAL = ''
 DEFAULT_STEPS = 24
+MAP_DIAMETER = 'diameter'
+MAP_STEPS = 'steps'
+MAP_FLIP = 'flip'
+MAP_MATERIAL = 'material'
+DELIMITER = ':'
+ID_DIAMETER = 'd'
+ID_STEPS = 's'
+ID_FLIP = 'f'
+CABLE_NAME_SUFFIX = '_geo'
 
 
 class CableLive:
@@ -81,32 +91,45 @@ class CableLive:
         return any((curves, beziers, bsplines))
 
 
+def meters(mm_str):
+    return float(mm_str) / 1000
+
+
 def decode_name(name):
-    pattern = r".*\[(\d+)?\s?:?\s?(.*)\]"
-    result: list[str] = re.findall(pattern, name)
-    if not result:
-        h3dd.print_debug(f'not result: {result = }')
-        return DEFAULT_DIAMETER, ''
-
-    if len(result) == 1:
-        if result[0].isdigit():
-            h3dd.print_debug(f'{len(result) = } {result = }')
-            return float(result[0]), ''
-
-        else:
-            h3dd.print_debug(f'else len: {len(result) = } {result = }')
-            return DEFAULT_DIAMETER, result[0]
-
+    pattern = r'(.*)\[(.*)\]'
+    matches = re.findall(pattern, name)
+    parameters = {
+        MAP_DIAMETER: DEFAULT_DIAMETER,
+        MAP_MATERIAL: DEFAULT_MATERIAL,
+        MAP_STEPS: DEFAULT_STEPS,
+        MAP_FLIP: False,
+    }
+    if not matches:
+        mesh_name = name
+        tokens = ''
     else:
-        h3dd.print_debug(f'else: {result = }')
-        return float(result[0]), result[1]
+        mesh_name = matches[0][0]
+        tokens = matches[0][1].split(':')
+    # tokens: str = name.split(':')
+    for token in tokens:
+        value = token.strip()
+        if value.isdigit() or value.startswith(ID_DIAMETER):
+            parameters[MAP_DIAMETER] = meters(value)
+        elif value.startswith(ID_STEPS):
+            parameters[MAP_STEPS] = int(value[1:])
+        elif value == ID_FLIP:
+            parameters[MAP_FLIP] = True
+        else:
+            parameters[MAP_MATERIAL] = value
+
+    return *parameters.values(), mesh_name
 
 
 def create_cable(curve_mesh, shape):
     if not CableLive.is_general_curve(curve_mesh):
         print(f'Cable creation skipped for mesh <{curve_mesh.name}>. No curve found.')
         return
-    # stroe preset browser status
+    # store preset browser status
     preset_browser_opened = lx.eval(
         'layout.createOrClose PresetBrowser presetBrowserPalette ?'
     )
@@ -115,8 +138,10 @@ def create_cable(curve_mesh, shape):
             'layout.createOrClose PresetBrowser presetBrowserPalette true Presets '
             'width:800 height:600 persistent:true style:palette'
         )
+    # get cable generation parameters
+    diameter, material_tag_name, steps, flip, name_str = decode_name(curve_mesh.name)
     # create curve sweep meshop
-    cable_name = f'{curve_mesh.name}_cable'
+    cable_name = f'{name_str.strip()}{CABLE_NAME_SUFFIX}'
     cable = scene.addMesh(cable_name)
     cable.select(replace=True)
     lx.eval(
@@ -155,26 +180,33 @@ def create_cable(curve_mesh, shape):
     )
     lx.eval('preset.do')
     lx.eval('item.channel poly.setType.meshop.item$type subd')
+    # create math multiply channel modifier
+    lx.eval('select.filepath "[itemtypes]:ChannelModifiers/math/cmMathBasic(mul).itemtype" set')
+    lx.eval('select.preset "[itemtypes]:ChannelModifiers/math/cmMathBasic(mul).itemtype" mode:set')
+    lx.eval('preset.do')
+    math_mult = scene.selectedByType(itype='cmMathBasic')[0]
     # create controls
-    diameter, material_tag_name = decode_name(curve_mesh.name)
-    material_tag_name = material_tag_name.strip()
     cable.select(replace=True)
     lx.eval(
         f'channel.create shapesize distance default:{diameter} username:"shape size"'
     )
-    lx.eval('channel.create flip boolean username:flip')
+    lx.eval('channel.create shapescale percent default:112.0 username:"shape compensation"')
     lx.eval(
         'channel.create cablesteps integer scalar false 0.0 false 0.0 0.0 username:"cable steps"'
     )
-    lx.eval(f'item.channel cablesteps "{DEFAULT_STEPS}"')
+    lx.eval(f'channel.create flip boolean default:{float(flip)} username:flip')
+    lx.eval(f'item.channel cablesteps "{steps}"')
     lx.eval('channel.create materialtag string username:"material tag"')
     lx.eval(f'item.channel materialtag "{material_tag_name}"')
-    lx.eval(f'channel.link add {{{cable.id}:shapesize}} {{{curve_sweep.id}:size}}')
     lx.eval(f'channel.link add {{{cable.id}:flip}} {{{curve_sweep.id}:flip}}')
     lx.eval(f'channel.link add {{{cable.id}:cablesteps}} {{{curve_sweep.id}:steps}}')
     lx.eval(
         f'channel.link add {{{cable.id}:materialtag}} {{{material_tag_node.id}:materialName}}'
     )
+    lx.eval(f'channel.link add {{{cable.id}:shapescale}} {{{math_mult.id}:input2}}')
+    lx.eval(f'channel.link add {{{cable.id}:shapesize}} {{{math_mult.id}:input1}}')
+    lx.eval(f'channel.link replace {{{math_mult.id}:output}} {{{curve_sweep.id}:size}}')
+
     # restore preset browser status
     if not preset_browser_opened:
         lx.eval(
@@ -186,6 +218,9 @@ def create_cable(curve_mesh, shape):
 def main():
     selected_meshes = scene.selectedByType(itype=c.MESH_TYPE)
     cable_shape = CableLive.get_cable_shape()
+    visible_channel = cable_shape.channel('visible')
+    if visible_channel:
+        visible_channel.set('allOff')
     for mesh in selected_meshes:
         # function to create live cable
         create_cable(curve_mesh=mesh, shape=cable_shape)
@@ -193,5 +228,5 @@ def main():
 
 if __name__ == '__main__':
     scene = modo.Scene()
-    h3dd = H3dDebug(enable=True, file=replace_file_ext(scene.filename, ".log"))
+    h3dd = H3dDebug(enable=False, file=replace_file_ext(scene.filename, ".log"))
     main()
