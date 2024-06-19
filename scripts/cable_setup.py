4561e93286cdd75a222013699fe9b94929de6270
Dmytro Holub <heap3d@gmail.com>
2024-06-20 01:37:40 +0300

    create cable 20240620

--- scripts/cable_setup.py
+++ scripts/cable_setup.py
@@ -13,7 +13,6 @@ import lx
 import modo
 import modo.constants as c
 import re
-from dataclasses import dataclass
 from typing import Union
 
 from h3d_utilites.scripts.h3d_utils import replace_file_ext
@@ -27,11 +26,11 @@ DELIMITER = ':'
 
 DEFAULT_CABLE_BASENAME = ''
 DEFAULT_DIAMETER = 0.01
-DEFAULT_COMPENSATION = 123.08
+DEFAULT_COMPENSATION = 1.2308
 DEFAULT_MATERIAL_TAG = ''
 DEFAULT_PATH_STEPS = 24
 DEFAULT_POLYGON_TYPE = 1
-DEFAULT_FLIP = False
+DEFAULT_FLIP = True
 DEFAULT_SIDES = 6
 
 ID_DIAMETER = 'd'
@@ -40,32 +39,31 @@ ID_FLIP = 'f'
 ID_POLYGON_TYPE = 't'
 ID_SIDES = 'p'
 
-CH_DIAMETER_NAME = 'diameter'
+CH_DIAMETER = 'ctrldiameter'
 CH_DIAMETER_USERNAME = 'Diameter'
-CH_COMP_NAME = 'compensation'
+CH_COMP = 'ctrlcompensation'
 CH_COMPENSATION_USERNAME = 'Diameter Compensation'
-CH_STEPS_NAME = 'steps'
+CH_STEPS = 'ctrlsteps'
 CH_STEPS_USERNAME = 'Steps'
-CH_SIDES_NAME = 'sides'
+CH_SIDES = 'ctrlsides'
 CH_SIDES_USERNAME = 'Sides'
-CH_PTAG_NAME = 'materialtag'
+CH_PTAG = 'ctrlmaterialtag'
 CH_MATERIAL_TAG_USERNAME = 'Material Name'
-CH_FLIP_NAME = 'flip'
+CH_FLIP = 'ctrlflip'
 CH_FLIP_USERNAME = 'Flip'
-CH_POLYGON_TYPE_NAME = 'polytype'
+CH_POLYGON_TYPE = 'ctrlpolytype'
 CH_POLYGON_TYPE_USERNAME = 'Polygon Type (0-2)'
 
 CMD_INDEPENDENT_PROFILE = 'independent'
 
 
-@dataclass
 class CableParams:
     diameter: float = DEFAULT_DIAMETER
     compensation: float = DEFAULT_COMPENSATION
     polygon_type: int = DEFAULT_POLYGON_TYPE
     steps: float = DEFAULT_PATH_STEPS
     sides = DEFAULT_SIDES
-    flip: bool = DEFAULT_FLIP
+    flip: bool = True
     material_name: str = DEFAULT_MATERIAL_TAG
     basename = DEFAULT_CABLE_BASENAME
 
@@ -76,6 +74,7 @@ class CableLive:
         self.curve_mesh: Union[modo.Item, None] = None
         self.cable_mesh: Union[modo.Item, None] = None
         self.profile_mesh: Union[modo.Item, None] = None
+        self.prim_cylinder_item: Union[modo.Item, None] = None
         self.curve_sweep_mop: Union[modo.Item, None] = None
         self.set_polygon_type_mop: Union[modo.Item, None] = None
         self.material_tag_mop: Union[modo.Item, None] = None
@@ -91,17 +90,17 @@ class CableLive:
         ...
 
     def strip_cable_setup(self):
-        scene.removeItems(self.cable_mesh)
+        modo.Scene().removeItems(self.cable_mesh)
         self.cable_mesh = None
-        scene.removeItems(self.curve_sweep_mop)
+        modo.Scene().removeItems(self.curve_sweep_mop)
         self.curve_sweep_mop = None
-        scene.removeItems(self.set_polygon_type_mop)
+        modo.Scene().removeItems(self.set_polygon_type_mop)
         self.set_polygon_type_mop = None
-        scene.removeItems(self.material_tag_mop)
+        modo.Scene().removeItems(self.material_tag_mop)
         self.material_tag_mop = None
-        scene.removeItems(self.shareable_profile_mesh)
+        modo.Scene().removeItems(self.shareable_profile_mesh)
         self.shareable_profile_mesh = None
-        scene.removeItems(self.math_mult_chmod)
+        modo.Scene().removeItems(self.math_mult_chmod)
         self.math_mult_chmod = None
 
     @staticmethod
@@ -112,12 +111,7 @@ class CableLive:
 
         return any((curves, beziers, bsplines))
 
-    def new_profile_mesh(self, name: str) -> modo.Item:
-        lx.eval('layer.new')
-        profile: modo.Item
-        profile = scene.selectedByType(c.MESH_TYPE)[0]
-        if not profile:
-            raise RuntimeError('Error creating profile mesh')
+    def create_prim_cylinder_item(self) -> modo.Item:
         lx.eval('select.preset "[itemtypes]:MeshOperations/create/primitives/prim.cylinder.item.itemtype" mode:set')
         lx.eval('select.filepath "[itemtypes]:MeshOperations/create/primitives/prim.cylinder.item.itemtype" set')
         lx.eval('preset.do')
@@ -130,13 +124,36 @@ class CableLive:
         lx.eval('item.channel prim.cylinder.item$segments 1')
         lx.eval(f'item.channel prim.cylinder.item$sides {self.params.sides}')
         lx.eval('item.channel prim.cylinder.item$polType face')
+
+        return modo.Scene().selectedByType('prim.cylinder.item')[0]
+
+    def new_profile_mesh(self, name: str) -> modo.Item:
+        lx.eval('layer.new')
+        profile: modo.Item
+        profile = modo.Scene().selectedByType(c.MESH_TYPE)[0]
+        if not profile:
+            raise RuntimeError('Error creating profile mesh')
+
+        self.prim_cylinder_item = self.create_prim_cylinder_item()
         profile.name = name
 
         return profile
 
+    def get_prim_cylinder_item(self, profile_mesh: modo.Item) -> modo.Item:
+        deformers: list[modo.Item] = profile_mesh.itemGraph('deformers').reverse()  # type: ignore
+        for deformer in deformers:
+            if deformer.type == 'prim.cylinder.item':
+                return deformer
+
+        connected_prim = self.create_prim_cylinder_item()
+        lx.eval(f'item.link genInfluence {profile_mesh.id} {connected_prim.id} posT:0 replace:false')
+        return connected_prim
+
     def get_shareable_profile(self, name: str) -> modo.Item:
         try:
-            return scene.item(name)
+            shareable_profile_mesh = modo.Scene().item(name)
+            self.prim_cylinder_item = self.get_prim_cylinder_item(shareable_profile_mesh)
+            return shareable_profile_mesh
         except LookupError:
             return self.new_profile_mesh(name)
 
@@ -164,8 +181,11 @@ class CableLive:
                 self.params.diameter = self.meters(token_stripped[len(ID_DIAMETER):])
             elif token_stripped.startswith(ID_STEPS):
                 self.params.steps = int(token_stripped[len(ID_STEPS):])
-            elif token_stripped == ID_FLIP:
-                self.params.flip = True
+            elif token_stripped.startswith(ID_FLIP):
+                if token_arg := token_stripped[len(ID_FLIP):]:
+                    self.params.flip = int(token_arg) != 0
+                else:
+                    self.params.flip = True
             elif token_stripped.startswith(ID_POLYGON_TYPE):
                 self.params.polygon_type = int(token_stripped[len(ID_POLYGON_TYPE):])
             elif token_stripped.startswith(ID_SIDES):
@@ -175,7 +195,7 @@ class CableLive:
 
     def create_cable_mesh(self) -> None:
         cable_name = f'{self.params.basename}{CABLE_BASENAME_SUFFIX}'
-        self.cable_mesh = scene.addMesh(cable_name)
+        self.cable_mesh = modo.Scene().addMesh(cable_name)
 
     def create_curve_sweep_mop(self) -> None:
         if not self.cable_mesh:
@@ -184,7 +204,7 @@ class CableLive:
         lx.eval('select.filepath "[itemtypes]:MeshOperations/curve/curve.sweep.itemtype" set')
         lx.eval('select.preset "[itemtypes]:MeshOperations/curve/curve.sweep.itemtype" mode:set')
         lx.eval('preset.do')
-        self.curve_sweep_mop = scene.selectedByType(itype='curve.sweep')[0]
+        self.curve_sweep_mop = modo.Scene().selectedByType(itype='curve.sweep')[0]
         if not self.curve_sweep_mop:
             raise ValueError(f'{self.curve_sweep_mop = }')
         self.curve_sweep_mop.select(replace=True)
@@ -195,8 +215,7 @@ class CableLive:
         lx.eval('select.filepath "[itemtypes]:MeshOperations/polygon/poly.setType.meshop.item.itemtype" set')
         lx.eval('select.preset "[itemtypes]:MeshOperations/polygon/poly.setType.meshop.item.itemtype" mode:set')
         lx.eval('preset.do')
-        # lx.eval('item.channel poly.setType.meshop.item$type subd')
-        self.material_tag_mop = scene.selectedByType(itype='pmodel.materialTag.item')[0]
+        self.set_polygon_type_mop = modo.Scene().selectedByType(itype='poly.setType.meshop.item')[0]
 
     def create_material_tag_mop(self) -> None:
         if not self.cable_mesh:
@@ -206,27 +225,27 @@ class CableLive:
         lx.eval('select.filepath "[itemtypes]:MeshOperations/polygon/pmodel.materialTag.item.itemtype" set')
         lx.eval('select.preset "[itemtypes]:MeshOperations/polygon/pmodel.materialTag.item.itemtype" mode:set')
         lx.eval('preset.do')
-        self.material_tag_mop = scene.selectedByType(itype='pmodel.materialTag.item')[0]
+        self.material_tag_mop = modo.Scene().selectedByType(itype='pmodel.materialTag.item')[0]
 
     def create_math_multiply_channel_mod(self) -> None:
         lx.eval('select.filepath "[itemtypes]:ChannelModifiers/math/cmMathBasic(mul).itemtype" set')
         lx.eval('select.preset "[itemtypes]:ChannelModifiers/math/cmMathBasic(mul).itemtype" mode:set')
         lx.eval('preset.do')
-        self.create_math_multiply_channel_mod = scene.selectedByType(itype='cmMathBasic')[0]
+        self.math_mult_chmod = modo.Scene().selectedByType(itype='cmMathBasic')[0]
 
-    def create_controls(self) -> None:
+    def create_cable_controls(self) -> None:
         if not self.cable_mesh:
             raise ValueError(f'{self.cable_mesh = }')
         self.cable_mesh.select(replace=True)
-        lx.eval(f'channel.create {CH_DIAMETER_NAME} distance username:"{CH_DIAMETER_USERNAME}"')
-        lx.eval(f'channel.create {CH_COMP_NAME} percent username:"{CH_COMPENSATION_USERNAME}"')
-        lx.eval(f'channel.create {CH_POLYGON_TYPE_NAME} integer username:"{CH_POLYGON_TYPE_USERNAME}"')
-        lx.eval(f'channel.create {CH_STEPS_NAME} integer username:"{CH_STEPS_USERNAME}"')
-        lx.eval(f'channel.create {CH_SIDES_NAME} integer username:"{CH_SIDES_USERNAME}"')
-        lx.eval(f'channel.create {CH_FLIP_NAME} boolean username:"{CH_FLIP_USERNAME}"')
-        lx.eval(f'channel.create {CH_PTAG_NAME} string username:"{CH_MATERIAL_TAG_USERNAME}"')
+        lx.eval(f'channel.create {CH_DIAMETER} distance username:"{CH_DIAMETER_USERNAME}"')
+        lx.eval(f'channel.create {CH_COMP} percent username:"{CH_COMPENSATION_USERNAME}"')
+        lx.eval(f'channel.create {CH_POLYGON_TYPE} integer username:"{CH_POLYGON_TYPE_USERNAME}"')
+        lx.eval(f'channel.create {CH_STEPS} integer username:"{CH_STEPS_USERNAME}"')
+        lx.eval(f'channel.create {CH_SIDES} integer username:"{CH_SIDES_USERNAME}"')
+        lx.eval(f'channel.create {CH_FLIP} boolean username:"{CH_FLIP_USERNAME}"')
+        lx.eval(f'channel.create {CH_PTAG} string username:"{CH_MATERIAL_TAG_USERNAME}"')
 
-    def link_channels(self) -> None:
+    def link_cable_channels(self) -> None:
         if not self.curve_sweep_mop:
             raise ValueError(f'{self.curve_sweep_mop = }')
         if not self.cable_mesh:
@@ -235,27 +254,34 @@ class CableLive:
             raise ValueError(f'{self.curve_mesh = }')
         if not self.profile_mesh:
             raise ValueError(f'{self.profile_mesh = }')
+        if not self.prim_cylinder_item:
+            raise ValueError(f'{self.prim_cylinder_item = }')
         if not self.math_mult_chmod:
             raise ValueError(f'{self.math_mult_chmod = }')
         if not self.material_tag_mop:
             raise ValueError(f'{self.material_tag_mop = }')
-        lx.eval(f'channel.link add {{{self.cable_mesh.id}:{CH_DIAMETER_NAME}}} {{{self.math_mult_chmod.id}:input1}}')
-        lx.eval(f'channel.link add {{{self.cable_mesh.id}:{CH_COMP_NAME}}} {{{self.math_mult_chmod.id}:input2}}')
+        if not self.set_polygon_type_mop:
+            raise ValueError(f'{self.set_polygon_type_mop = }')
+
+        lx.eval(f'channel.link add {{{self.cable_mesh.id}:{CH_DIAMETER}}} {{{self.math_mult_chmod.id}:input1}}')
+        lx.eval(f'channel.link add {{{self.cable_mesh.id}:{CH_COMP}}} {{{self.math_mult_chmod.id}:input2}}')
         lx.eval(f'channel.link replace {{{self.math_mult_chmod.id}:output}} {{{self.curve_sweep_mop.id}:size}}')
         lx.eval(f'item.link curve.sweep.path {self.curve_mesh.id} {self.curve_sweep_mop.id} posT:0 replace:false')
         lx.eval(f'item.link curve.sweep.prof {self.profile_mesh.id} {self.curve_sweep_mop.id} posT:0 replace:false')
-        lx.eval(f'channel.link add {{{self.cable_mesh.id}:{CH_STEPS_NAME}}} {{{self.curve_sweep_mop.id}:steps}}')
-        lx.eval(f'channel.link add {{{self.cable_mesh.id}:{CH_FLIP_NAME}}} {{{self.curve_sweep_mop.id}:flip}}')
-        lx.eval(f'channel.link add {{{self.cable_mesh.id}:{CH_PTAG_NAME}}} {{{self.material_tag_mop.id}:materialName}}')
+        lx.eval(f'channel.link add {{{self.cable_mesh.id}:{CH_STEPS}}} {{{self.curve_sweep_mop.id}:steps}}')
+        lx.eval(f'channel.link add {{{self.cable_mesh.id}:{CH_FLIP}}} {{{self.curve_sweep_mop.id}:flip}}')
+        lx.eval(f'channel.link add {{{self.cable_mesh.id}:{CH_POLYGON_TYPE}}} {{{self.set_polygon_type_mop.id}:type}}')
+        lx.eval(f'channel.link add {{{self.cable_mesh.id}:{CH_PTAG}}} {{{self.material_tag_mop.id}:materialName}}')
+        lx.eval(f'channel.link add {{{self.cable_mesh.id}:{CH_SIDES}}} {{{self.prim_cylinder_item.id}:sides}}')
 
-    def set_control_channels(self) -> None:
-        lx.eval(f'item.channel {CH_DIAMETER_NAME} "{self.params.diameter}"')
-        lx.eval(f'item.channel {CH_COMP_NAME} "{self.params.compensation}"')
-        lx.eval(f'item.channel {CH_POLYGON_TYPE_NAME} "{self.params.polygon_type}"')
-        lx.eval(f'item.channel {CH_STEPS_NAME} "{self.params.steps}"')
-        lx.eval(f'item.channel {CH_SIDES_NAME} "{self.params.sides}"')
-        lx.eval(f'item.channel {CH_FLIP_NAME} "{self.params.flip}"')
-        lx.eval(f'item.channel {CH_PTAG_NAME} "{self.params.material_name}"')
+    def set_cable_control_channels(self) -> None:
+        lx.eval(f'item.channel {CH_DIAMETER} "{self.params.diameter}"')
+        lx.eval(f'item.channel {CH_COMP} "{self.params.compensation}"')
+        lx.eval(f'item.channel {CH_POLYGON_TYPE} "{self.params.polygon_type}"')
+        lx.eval(f'item.channel {CH_STEPS} "{self.params.steps}"')
+        lx.eval(f'item.channel {CH_SIDES} "{self.params.sides}"')
+        lx.eval(f'item.channel {CH_FLIP} "{self.params.flip}"')
+        lx.eval(f'item.channel {CH_PTAG} "{self.params.material_name}"')
 
     def create_live_cable(self, is_profile_independent: bool) -> None:
         if not self.curve_mesh:
@@ -286,9 +312,9 @@ class CableLive:
         self.create_material_tag_mop()
         self.create_set_polygon_type_mop()
         self.create_math_multiply_channel_mod()
-        self.create_controls()
-        self.link_channels()
-        self.set_control_channels()
+        self.create_cable_controls()
+        self.link_cable_channels()
+        self.set_cable_control_channels()
 
         # restore preset browser status
         if not preset_browser_opened:
@@ -300,7 +326,7 @@ class CableLive:
 
 def main():
     is_profile_independent: bool = False
-    selected_meshes = scene.selectedByType(itype=c.MESH_TYPE)
+    selected_meshes = modo.Scene().selectedByType(itype=c.MESH_TYPE)
     args = lx.args()
     if args:
         if CMD_INDEPENDENT_PROFILE in args:
@@ -315,6 +341,5 @@ def main():
 
 
 if __name__ == '__main__':
-    scene = modo.Scene()
-    h3dd = H3dDebug(enable=False, file=replace_file_ext(scene.filename, ".log"))
+    h3dd = H3dDebug(enable=True, file=replace_file_ext(modo.Scene().filename, ".log"))
     main()
